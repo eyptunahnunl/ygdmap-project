db70199a7f137958e75e8a8a99f360075876fc0e
eyptunahnunl <e.tunahan.unal.05@gmail.com>
2023-08-20 16:53:40 +0300

    controller

--- src/main/java/com/tunadev/ygdmap/controller/geoJsonController.java
+++ src/main/java/com/tunadev/ygdmap/controller/geoJsonController.java
@@ -1,4 +1,29 @@
 package com.tunadev.ygdmap.controller;
 
+import com.tunadev.ygdmap.payload.DataRequest;
+import com.tunadev.ygdmap.payload.OtherShape;
+import com.tunadev.ygdmap.service.GeoJsonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class geoJsonController {
+
+    GeoJsonService geoJsonService;
+
+    @Autowired
+    public geoJsonController(GeoJsonService geoJsonService) {
+        this.geoJsonService = geoJsonService;
+    }
+
+    @CrossOrigin
+    @PostMapping("/data")
+    public ResponseEntity<OtherShape> postData(
+            @RequestBody DataRequest dataRequest) throws Exception {
+
+        return geoJsonService.findShapesWithinPolygon(dataRequest.getOtherShape(), dataRequest.getPolygon());
+    }
+
 }
